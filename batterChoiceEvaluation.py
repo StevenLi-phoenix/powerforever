0def647e0053ac6ca9f623221faaacde040f935b
lishuyu <lishuyustevenli@163.com>
2021-11-13 12:56:29 +0800

    update 11.13 noon

--- batterChoiceEvaluation.py
+++ batterChoiceEvaluation.py
@@ -20,9 +20,10 @@ class calculate:
         # self.max_recharge_rate = 7000 # 最大充电速率7kw
         # self.max_solar_generation_rate = 20000 # 最大太阳能发20kwh
 
-        self.threadLimiter = threading.BoundedSemaphore(50)
+        self.max_thread = 8
+        self.threadLimiter = threading.BoundedSemaphore(self.max_thread)
 
-        self.base = "/Users/lishuyu/PycharmProjects/solarPowerEstimate/"
+        self.base = "./"
         iter_file_path = f"{self.base}iter.csv"
         self.npy = f"{self.base}npy/"
         self.plot = f"{self.base}plot/"
@@ -54,13 +55,13 @@ class calculate:
                 ["max_nopower_count", "Evaluate Score", "name", "max_cap", "other_info", "year_round_status",
                  "current_cap"])
 
-    def evaluate(self, batteries=None):
+    def evaluate(self, batteries=None, ret = False):
 
         max_nopower_count = 0
 
         if batteries is None:
             batteries = [battery("default", 7000, 10000, 0.96, 100)]
-            self.log.write_error("No battery option input try default instead")
+            # self.log.write_error("No battery option input try default instead")
         current_cap = []
         status = True
         year_round_status = []
@@ -93,16 +94,14 @@ class calculate:
 
                 # if power can not suppply the use of user
                 if pull > 0:
-                    self.log.write(
-                        f"{charge}, {discharge}, {sum([i.capacity for i in batteries])}, note: battery is later for one")
-                    self.log.write(f"No avaliable energy on day:{hour // 24} hour:{hour}")
+                    # self.log.write(f"{charge}, {discharge}, {sum([i.capacity for i in batteries])}, note: battery is later for one")
+                    # self.log.write(f"No avaliable energy on day:{hour // 24} hour:{hour}")
                     # if not status: pass
                     if status:
                         max_nopower_count += 1
                         # set status to false and max_count -1
                         status = False
-                        self.log.write(
-                            f"No avaliable energy on day:{hour // 24}, {sum([i.capacity for i in batteries])}, count_increase, set status to false")
+                        # self.log.write( f"No avaliable energy on day:{hour // 24}, {sum([i.capacity for i in batteries])}, count_increase, set status to false")
 
             else:
                 self.log.write("balance")
@@ -111,13 +110,16 @@ class calculate:
             else:
                 year_round_status.append(0)
         # raise KeyboardInterrupt("I am down")
-        self.threadLimiter.release()
         # "max_nopower_count", "Evaluate Score", "name","max_cap","other_info", "year_round_status", "current_cap"
-        score = self.choice_analysis()
-        self.write_list.append([max_nopower_count, score, json.dumps([i.name for i in batteries]),
-                                sum([i.max_capactity for i in batteries]), json.dumps([i.info() for i in batteries]),
-                                json.dumps(year_round_status), json.dumps(current_cap)])
-        # return year_round_status, current_cap, max_nopower_count
+        if max_nopower_count <= 10:
+            score = self.choice_analysis()
+            self.write_list.append([max_nopower_count, score, json.dumps([i.name for i in batteries]),
+                                    sum([i.max_capactity for i in batteries]), json.dumps([i.info() for i in batteries]),
+                                    json.dumps(year_round_status), json.dumps(current_cap)])
+        self.threadLimiter.release()
+        # print("End thread")
+        print("\t"+str(max_nopower_count), end="      ")
+        if ret: return year_round_status, current_cap, max_nopower_count
 
     def choice_analysis(self):
         """
@@ -133,12 +135,20 @@ class calculate:
         """
         while True:
             time.sleep(1)
-            if len(self.write_list) > 100:
+            if len(self.write_list) > 10:
                 with open("battery_evaluate_ouput.csv", "a+") as f:
                     write_list_copy = self.write_list
                     self.write_list = []
                     writer = csv.writer(f)
                     writer.writerows(write_list_copy)
+    def write_all(self):
+        for i in range(self.max_thread):
+            self.threadLimiter.acquire()
+        with open("battery_evaluate_ouput.csv", "a+") as f:
+            write_list_copy = self.write_list
+            self.write_list = []
+            writer = csv.writer(f)
+            writer.writerows(write_list_copy)
 
     def main(self):
         """
@@ -163,7 +173,9 @@ class calculate:
             self.threadLimiter.acquire()
             t = threading.Thread(target=self.evaluate, args=(bats,))
             threads.append(t)
+            print(f"\rStart thread {t}", end="")
             t.start()
+        self.write_all()
 
 
 class battery:
